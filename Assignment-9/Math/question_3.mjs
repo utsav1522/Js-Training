3d59334968605c1d1846dfd4ef827b6d5964c8c5
utsav1522 <utsav.jain@successive.tech>
2024-03-27 14:21:55 +0530

    Assignment-9 commit question 2 and 3

---
+++ Assignment-9/Math/question_3.mjs
@@ -0,0 +1,4 @@
+import * as Math from "./Math.mjs";
+
+console.log("Sum of 2 numbers is : ", Math.add(10, 20));
+console.log("Difference between the numbers is : ", Math.subtract(20, 10));
---
+++ Assignment-9/question_2.js
@@ -0,0 +1,3 @@
+const { mean } = require("lodash");
+const arr = [1, 2, 3, 4, 5];
+console.log(mean(arr));
